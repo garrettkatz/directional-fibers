e8fcbf5b7d11b2df4e5b184721155c4833cf7d73
garrettkatz <garrett.katz@gmail.com>
2023-11-25 16:34:52 -0500

    saving qubit fiber results

--- dfibers/examples/qubits.py
+++ dfibers/examples/qubits.py
@@ -1,4 +1,6 @@
+import sys
 import itertools as it
+import pickle as pk
 import numpy as np
 import torch as tr
 import matplotlib as mp
@@ -11,6 +13,7 @@ from dfibers.logging_utilities import Logger
 
 np.set_printoptions(linewidth=1000)
 tr.set_printoptions(linewidth=1000)
+tr.set_default_dtype(tr.float64)
 
 # helper to kron sequence of matrices
 def kron(*args):
@@ -35,7 +38,7 @@ pauli = tr.tensor([
 n_qb = 6
 
 # form all of the kroneker products
-pauli_prods = tr.zeros(9, 2**n_qb, 2**n_qb, dtype=tr.cfloat)
+pauli_prods = tr.zeros(9, 2**n_qb, 2**n_qb, dtype=tr.complex128)
 p = 0
 for a, b in it.product(range(4), range(1,4)):
     if (a,b) in ((0,1), (0,2), (1,1)): continue
@@ -54,30 +57,40 @@ def get_spectrum(c):
     spectrum = tr.linalg.eigvalsh(H) # (K, 2**n_qb)
     return spectrum
 
-c0 = tr.randn(1,9)
-target_spectrum = get_spectrum(c0)
+def get_loss_factory(c0):
+    target_spectrum = get_spectrum(c0)
 
-def get_loss(c):
-    # c is (K, 9) tensor, K is batch size
-    spectrum = get_spectrum(c)
-    return tr.sum((spectrum - target_spectrum)**2, dim=1) # (K,)
+    def get_loss(c):
+        # c is (K, 9) tensor, K is batch size
+        spectrum = get_spectrum(c)
+        return tr.sum((spectrum - target_spectrum)**2, dim=1) # (K,)
 
-def f(v):
-    # v is (9, K) numpy array, return (9, K) batch of gradients
-    c = tr.tensor(v.T, requires_grad=True) # torch wants batch first
-    loss = get_loss(c)
-    loss.sum().backward()
-    return c.grad.numpy().T
+    return get_loss
 
-def Df(v):
-    # v is (9, K) numpy array, return (K, 9, 9) batch of hessians (jacobian of gradients)
-    hess_fun = tr.func.hessian(get_loss)
-    Dfv = []
-    for k in range(v.shape[1]):
-        c = tr.tensor(v[:,k:k+1].T)
-        Dfv.append(hess_fun(c).squeeze()) # squeeze singleton batch dimensions
-    Dfv = tr.stack(Dfv)
-    return Dfv.numpy()
+def f_factory(get_loss):
+
+    def f(v):
+        # v is (9, K) numpy array, return (9, K) batch of gradients
+        c = tr.tensor(v.T, requires_grad=True) # torch wants batch first
+        loss = get_loss(c)
+        loss.sum().backward()
+        return c.grad.numpy().T
+
+    return f
+
+def Df_factory(get_loss):
+
+    def Df(v):
+        # v is (9, K) numpy array, return (K, 9, 9) batch of hessians (jacobian of gradients)
+        hess_fun = tr.func.hessian(get_loss)
+        Dfv = []
+        for k in range(v.shape[1]):
+            c = tr.tensor(v[:,k:k+1].T)
+            Dfv.append(hess_fun(c).squeeze()) # squeeze singleton batch dimensions
+        Dfv = tr.stack(Dfv)
+        return Dfv.numpy()
+
+    return Df
 
 def ef(v): return 1e-9
 
@@ -86,62 +99,107 @@ if __name__ == "__main__":
     mp.rcParams['font.family'] = 'serif'
     mp.rcParams['text.usetex'] = True
 
-    # get initial point
-    v0 = c0.numpy().T
-    
-    # Set up fiber arguments
-    fiber_kwargs = {
-        "f": f,
-        "ef": ef,
-        "Df": Df,
-        "compute_step_amount": lambda trace: (0.1, 0, False),
-        "v": v0,
-        "c": None, # f(v0) = 0, so use default random choice of direction vector
-        "terminate": None,
-        "max_step_size": 1,
-        "max_traverse_steps": 50,#000,
-        "max_solve_iterations": 2**5,
-        # "logger": logger,
-    }
-    
-    # Run in one direction
-    solution = sv.fiber_solver(**fiber_kwargs)
-    X1 = np.concatenate(solution["Fiber trace"].points, axis=1)
-    V1 = X1[:-1,:]
-    A1 = X1[-1,:]
-    R1 = solution["Fixed points"]
-    z = solution["Fiber trace"].z_initial
-    print(len(A1))
-    
-    # Run in other direction (negate initial tangent)
-    fiber_kwargs["z"] = -z
-    solution = sv.fiber_solver(**fiber_kwargs)
-    X2 = np.concatenate(solution["Fiber trace"].points, axis=1)
-    V2 = X2[:-1,:]
-    A2 = X2[-1,:]
-    R2 = solution["Fixed points"]
-    print(len(A2))
-    
-    # Join fiber segments and roots
-    V = np.concatenate((np.fliplr(V1), V2), axis=1)
-    A = np.concatenate((A1[::-1], A2), axis=0)
-    R = np.concatenate((R1, R2), axis=1)    
-    C = f(V)
+    do_fiber = False
 
-    # duplicates = lambda U, v: (np.fabs(U - v) < 0.1).all(axis=0)
+    if do_fiber:
+
+        # random sample for target spectrum
+        c_targ = tr.randn(1,9)
+        get_loss = get_loss_factory(c_targ)
+        f = f_factory(get_loss)
+        Df = Df_factory(get_loss)
+
+        # get initial fiber point
+        v0 = c_targ.numpy().T
+
+        # Set up fiber arguments
+        fiber_kwargs = {
+            "f": f,
+            "Df": Df,
+            "ef": ef,
+            "compute_step_amount": lambda trace: (0.0005, 0, False),
+            "v": v0,
+            "c": None, # f(v0) = 0, so use default random choice of direction vector
+            "terminate": None,
+            "max_step_size": 1,
+            "max_traverse_steps": 2000,#000,
+            "max_solve_iterations": 2**5,
+            "logger": Logger(sys.stdout),
+        }
+
+        # Run in one direction
+        solution = sv.fiber_solver(**fiber_kwargs)
+        X1 = np.concatenate(solution["Fiber trace"].points, axis=1)
+        V1 = X1[:-1,:]
+        A1 = X1[-1,:]
+        R1 = solution["Fixed points"]
+        z = solution["Fiber trace"].z_initial
+        print(len(A1))
+
+        # Run in other direction (negate initial tangent)
+        fiber_kwargs["z"] = -z
+        solution = sv.fiber_solver(**fiber_kwargs)
+        X2 = np.concatenate(solution["Fiber trace"].points, axis=1)
+        V2 = X2[:-1,:]
+        A2 = X2[-1,:]
+        R2 = solution["Fixed points"]
+        print(len(A2))
+
+        # Join fiber segments and roots
+        V = np.concatenate((np.fliplr(V1), V2), axis=1)
+        A = np.concatenate((A1[::-1], A2), axis=0)
+        R = np.concatenate((R1, R2), axis=1)
+        C = f(V)
+
+        R, fixed = fx.refine_points(R, f, ef, Df)
+        R = R[:, fixed]
+
+        duplicates = lambda U, v: (np.fabs(U - v) < 1e-3).all(axis=0)
+        R = fx.get_unique_points(R, duplicates)
+
+        with open("qubits.pkl","wb") as f: pk.dump((c_targ, V, A, R), f)
+
+    with open("qubits.pkl","rb") as f: (c_targ, V, A, R) = pk.load(f)
+
+    get_loss = get_loss_factory(c_targ)
+    f = f_factory(get_loss)
+
+    # # filter duplicates
+    # duplicates = lambda U, v: (np.fabs(U - v) < 1e-3).all(axis=0)
     # R = fx.get_unique_points(R, duplicates)
 
-    R, fixed = fx.refine_points(R, f, ef, Df)
-    R = R[:, fixed]
-    print(f"{R.shape[1]} roots")
+    # filter stationary points with non-zero loss
+    loss = get_loss(tr.tensor(R.T))
+    keep = (loss.numpy() < 1e-7)
+    R = R[:,keep]
+    loss = get_loss(tr.tensor(R.T))
 
-    print(np.fabs(f(R)).max(axis=0))
+    print(f"{R.shape[1]} optima")
+
+    targ_spectrum = get_spectrum(c_targ)
+    spectrums = get_spectrum(tr.tensor(R.T))
+    diffs = spectrums - targ_spectrum
+
+    print(f"loss = {loss}")
+    print("f(R)", np.fabs(f(R)).max(axis=0))
     print(R)
 
-    loss = get_loss(tr.tensor(R.T))
-    print(f"loss = {loss}")
-
     pt.plot(A)
+    pt.show()
+
+    pt.figure(figsize=(10,4))
+
+    pt.subplot(1,2,1)
+    pt.plot(R)
+    pt.xlabel("$ab$")
+    pt.ylabel("$c_{ab}$")
+
+    pt.subplot(1,2,2)
+    pt.plot(diffs.numpy().T)
+    pt.xlabel("$i$")
+    pt.ylabel("$\\lambda_i - \\lambda^*_i$")
+
+    pt.tight_layout()
     pt.show()
 
     # pt.figure(figsize=(6.5, 3))
